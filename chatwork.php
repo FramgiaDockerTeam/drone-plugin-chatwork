b7dcf9f7269883397b7924c7fb94c7b0592644c0
TungPro <mr.nttung@gmail.com>
2016-09-26 14:19:34 +0700

    change room_id to array

--- chatwork.php
+++ chatwork.php
@@ -6,8 +6,6 @@ $arguments = json_decode($arguments, true);
 $vargs = $arguments['vargs'];
 
 if (isset($vargs['room_id']) && isset($vargs['token'])) {
-    $url = 'https://api.chatwork.com/v1/rooms/' . $vargs['room_id'] . '/messages';
-
     $defaultFormat = "[info][title]{repo.owner}/{repo.name}#{build.commit} {build.status}[/title]Branch: {build.branch}\r\nAuthor: {build.author}\r\nMessage: {build.message}\r\n{system.link_url}/{repo.full_name}/{build.number}[/info]";
 
     $format = isset($vargs['format']) ? $vargs['format'] : $defaultFormat;
@@ -17,7 +15,32 @@ if (isset($vargs['room_id']) && isset($vargs['token'])) {
     $header = [];
     $header[] = 'X-ChatWorkToken: ' . $vargs['token'];
     $params = ['body' => $body];
+    
+    $rooms = [];
+    $rooms = is_array($vargs['room_id']) ? $vargs['room_id'] : [$vargs['room_id']];
+    
+    foreach ($rooms as $room) {
+        echo 'Room ' . $room . ': ';
+        
+        $url = 'https://api.chatwork.com/v1/rooms/' . $room . '/messages';
+        if ($result = callCurl($url, $params, $header)) {
+            $res = json_decode($result, true);
+            if (isset($res['errors'])) {
+                echo 'ERROR: ' . implode($res['errors'], '|');
+            } else {
+                echo 'Sent successfully!';
+            }
+        } else {
+            echo 'Something error!' . $result;
+        }
+        echo "\r\n";
+    }
+} else {
+    echo 'room_id & token is required.';
+}
 
+function callCurl($url, $params, $header = [])
+{
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
     curl_setopt($ch, CURLOPT_POST, 1);
@@ -25,17 +48,9 @@ if (isset($vargs['room_id']) && isset($vargs['token'])) {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($ch);
     curl_close($ch);
-    if ($result) {
-        echo 'Sent successfully!';
-    } else {
-        echo 'Something error!' . result;
-    }
-} else {
-    echo 'room_id & token is required.';
+    
+    return $result;
 }
-
-die();
-
 function formatBody($format, $arguments)
 {
     return preg_replace_callback(
